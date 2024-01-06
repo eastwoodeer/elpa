2b8e401d32fdb87c3a192ca798bd2d122cf6186d
Chen Bin <chen.bin11@zte.com.cn>
2024-01-06 10:57:42 +0800

    update packages

--- bufferlo-0.5/bufferlo-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from bufferlo.el  -*- no-byte-compile: t -*-
-(define-package "bufferlo" "0.5" "Manage frame/tab-local buffer lists" '((emacs "27.1")) :commit "aa0129986f81e4d630c3cf41c9636cf0a72fdc91" :authors '(("Florian Rommel" . "mail@florommel.de")) :maintainer '("Florian Rommel" . "mail@florommel.de") :keywords '("buffer" "frame" "tabs" "local") :url "https://github.com/florommel/bufferlo")
