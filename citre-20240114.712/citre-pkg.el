ecacc1ea266f505cec639f7b61c0db98731583d4
Chen Bin <chen.bin11@zte.com.cn>
2024-01-20 13:42:42 +0800

    update packages

---
+++ citre-20240114.712/citre-pkg.el
@@ -0,0 +1,14 @@
+(define-package "citre" "20240114.712" "Ctags IDE on the True Editor"
+  '((emacs "26.1"))
+  :commit "a900987221930e03975178d2b75b6dcd2366013d" :authors
+  '(("Hao Wang" . "amaikinono@gmail.com"))
+  :maintainers
+  '(("Hao Wang" . "amaikinono@gmail.com"))
+  :maintainer
+  '("Hao Wang" . "amaikinono@gmail.com")
+  :keywords
+  '("convenience" "tools")
+  :url "https://github.com/universal-ctags/citre")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
