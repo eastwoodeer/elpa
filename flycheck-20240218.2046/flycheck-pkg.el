0c8a9d0b2f40f81c06f082d5d878472ece360424
Chen Bin <chen.bin11@zte.com.cn>
2024-02-19 11:38:23 +0800

    update @ 2024.02.19

---
+++ flycheck-20240218.2046/flycheck-pkg.el
@@ -0,0 +1,14 @@
+(define-package "flycheck" "20240218.2046" "On-the-fly syntax checking"
+  '((emacs "26.1"))
+  :commit "9854769249686cb23c79ec1fff0ac0735a0f7e81" :authors
+  '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
+  :maintainers
+  '(("Clément Pit-Claudel" . "clement.pitclaudel@live.com"))
+  :maintainer
+  '("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
+  :keywords
+  '("convenience" "languages" "tools")
+  :url "https://www.flycheck.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
