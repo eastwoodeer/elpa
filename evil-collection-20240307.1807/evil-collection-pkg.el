1c032572f302960ea795a757ea88df3ed19cf68b
Chen Bin <chen.bin11@zte.com.cn>
2024-03-09 13:52:13 +0800

    update @ 2024.03.09

---
+++ evil-collection-20240307.1807/evil-collection-pkg.el
@@ -0,0 +1,16 @@
+(define-package "evil-collection" "20240307.1807" "A set of keybindings for Evil mode"
+  '((emacs "26.3")
+    (evil "1.2.13")
+    (annalist "1.0"))
+  :commit "fe96b412f437f263f99492fe293499016b816a78" :authors
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers
+  '(("James Nguyen" . "james@jojojames.com"))
+  :maintainer
+  '("James Nguyen" . "james@jojojames.com")
+  :keywords
+  '("evil" "tools")
+  :url "https://github.com/emacs-evil/evil-collection")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
