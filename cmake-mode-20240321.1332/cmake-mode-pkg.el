0a76ed41a4d84510c4a6555ec1bab3a5e7bdcfa2
Chen Bin <chen.bin11@zte.com.cn>
2024-04-01 13:42:00 +0800

    update @ 2024.04.01

---
+++ cmake-mode-20240321.1332/cmake-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "cmake-mode" "20240321.1332" "major-mode for editing CMake sources"
+  '((emacs "24.1"))
+  :commit "25d2850c190ba915a7527a29dda6efe61be901f0")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ org-re-reveal-20240325.1420/org-re-reveal-pkg.el
@@ -0,0 +1,10 @@
+(define-package "org-re-reveal" "20240325.1420" "Org export to reveal.js presentations"
+  '((emacs "24.4")
+    (org "8.3")
+    (htmlize "1.34"))
+  :commit "1944ac53d4cdd0fb250cc575447d5e6320fd5f81" :keywords
+  '("tools" "outlines" "hypermedia" "slideshow" "presentation" "oer")
+  :url "https://gitlab.com/oer/org-re-reveal")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
