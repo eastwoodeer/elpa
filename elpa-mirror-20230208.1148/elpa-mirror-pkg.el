8721f67ecc1a050f95505c536dc67bfa8a19b138
Chen Bin <chen.bin11@zte.com.cn>
2024-04-24 09:05:50 +0800

    update @ 2024.04.24

--- elpa-mirror-20230208.1148/elpa-mirror-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from elpa-mirror.el  -*- no-byte-compile: t -*-
-(define-package "elpa-mirror" "20230208.1148" "Create local package repository from installed packages" '((emacs "25.1")) :commit "9d7cfbf72ef8c7cd014c91e5bb3d8fbebda56140" :authors '(("Chen Bin" . "chenbin.sh@gmail.com")) :maintainer '("Chen Bin" . "chenbin.sh@gmail.com") :keywords '("tools") :url "http://github.com/redguardtoo/elpa-mirror")
---
+++ emacsql-sqlite-20240415.1535/emacsql-sqlite-pkg.el
@@ -0,0 +1,13 @@
+(define-package "emacsql-sqlite" "20240415.1535" "EmacSQL back-end for SQLite"
+  '((emacs "25.1")
+    (emacsql "20230220"))
+  :commit "5108c16c5e1d5bfdd41fcc0807241e28886ab763" :authors
+  '(("Christopher Wellons" . "wellons@nullprogram.com"))
+  :maintainers
+  '(("Jonas Bernoulli" . "emacs.emacsql@jonas.bernoulli.dev"))
+  :maintainer
+  '("Jonas Bernoulli" . "emacs.emacsql@jonas.bernoulli.dev")
+  :url "https://github.com/magit/emacsql")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
