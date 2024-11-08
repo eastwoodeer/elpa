37b9fad2aac3ce2500302ade915e2b1b99e31907
Chen Bin <chen.bin11@zte.com.cn>
2024-11-08 09:08:43 +0800

    update 2024.11.08

--- dracula-theme-20240912.2032/dracula-theme-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "dracula-theme" "20240912.2032"
-  "Dracula Theme."
-  '((emacs "24.3"))
-  :url "https://github.com/dracula/emacs"
-  :commit "c1614c68bb7cb6f9a6f4f779f47cf0d24b938b6f"
-  :revdesc "c1614c68bb7c"
-  :maintainers '(("tienne Deparis" . "etienne@depar.is")))
---
+++ evil-matchit-20241107.843/evil-matchit-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "evil-matchit" "20241107.843"
+  "Vim matchit ported to Evil."
+  '((emacs "25.1"))
+  :url "https://github.com/redguardtoo/evil-matchit"
+  :commit "86d22498c8a2bcfae170f11b698bee935d577f07"
+  :revdesc "86d22498c8a2"
+  :keywords '("matchit" "vim" "evil")
+  :authors '(("Chen Bin" . "chenbin.sh@gmail.com"))
+  :maintainers '(("Chen Bin" . "chenbin.sh@gmail.com")))
