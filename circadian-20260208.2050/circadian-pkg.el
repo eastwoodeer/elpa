c5c6f47b28b1a1b243eb97cd90c3daa025f4a40c
Chenbin <chen.bin11@zte.com.cn>
2026-02-24 13:15:21 +0800

    update 2026.02.24

---
+++ circadian-20260208.2050/circadian-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "circadian" "20260208.2050"
+  "Theme-switching based on daytime."
+  '((emacs "27.2"))
+  :url "https://github.com/GuidoSchmidt/circadian"
+  :commit "181bb35ba9416bdede255c9fa6b86a2d60115f94"
+  :revdesc "181bb35ba941"
+  :keywords '("themes")
+  :maintainers '(("Guido Schmidt" . "git@guidoschmidt.cc")))
--- srcery-theme-20250901.1751/srcery-theme-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "srcery-theme" "20250901.1751"
-  "Dark color theme."
-  '((emacs "24"))
-  :url "https://github.com/srcery-colors/srcery-emacs"
-  :commit "eee86865bb7baa8f015ab57a42d9c12e51b8b6fb"
-  :revdesc "eee86865bb7b"
-  :keywords '("faces"))
