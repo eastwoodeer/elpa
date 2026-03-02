65f3aeffbd75a03030926a90895e59a27b56c609
Chenbin <chen.bin11@zte.com.cn>
2026-03-02 12:53:19 +0800

    update 10 packages

---
+++ gptel-20260228.1148/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20260228.1148"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.7.4")
+    (compat    "30.1.0.0"))
+  :url "https://github.com/karthink/gptel"
+  :commit "63795dee75db49a04fd87842a1dcdef29c855f93"
+  :revdesc "63795dee75db"
+  :keywords '("convenience" "tools")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
