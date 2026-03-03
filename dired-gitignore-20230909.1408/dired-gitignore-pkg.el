b8ba7eef9776d01da29bca376fa107fc6de5c18e
eastwoodeer <eastwoodeer@outlook.com>
2026-03-03 22:15:14 +0800

    add dired-gitignore

---
+++ dired-gitignore-20230909.1408/dired-gitignore-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "dired-gitignore" "20230909.1408"
+  "A minor mode to hide gitignored files in a dired buffer."
+  '((emacs "27.1"))
+  :url "https://github.com/johannes-mueller/dired-gitignore.el"
+  :commit "b6e804c212b497d5502600bd4df10196e44d2bf7"
+  :revdesc "b6e804c212b4"
+  :keywords '("dired" "convenience" "git")
+  :authors '(("Johannes Mueller" . "github@johannes-mueller.org"))
+  :maintainers '(("Johannes Mueller" . "github@johannes-mueller.org")))
