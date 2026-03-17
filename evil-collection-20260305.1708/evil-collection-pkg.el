195dc95053e6f4bdaccb29dce55ba536575dd8bf
陈彬10090165 <chen.bin11@zte.com.cn>
2026-03-17 16:21:47 +0800

    update packages and add paredit and evil-cleverparens

---
+++ evil-collection-20260305.1708/evil-collection-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "evil-collection" "20260305.1708"
+  "A set of keybindings for Evil mode."
+  '((emacs    "26.3")
+    (evil     "1.2.13")
+    (annalist "1.0"))
+  :url "https://github.com/emacs-evil/evil-collection"
+  :commit "0c63456ae73520839b66c3985de9bfabd38b835f"
+  :revdesc "0c63456ae735"
+  :keywords '("evil" "tools")
+  :authors '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers '(("James Nguyen" . "james@jojojames.com")))
---
+++ racket-mode-20260303.1556/racket-mode-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "racket-mode" "20260303.1556"
+  "Racket editing, REPL, and more."
+  '((emacs  "25.1")
+    (compat "30.0.2.0"))
+  :url "https://www.racket-mode.com/"
+  :commit "24cb8e977a05d188756284e46ff7cece2d899f75"
+  :revdesc "24cb8e977a05"
+  :authors '(("Greg Hendershott" . "racket-mode-author@greghendershott.com")))
