e251e58eb8b79b4683f407f48e2d9b4c3539350e
eastwoodeer <eastwoodeer@outlook.com>
2026-04-16 22:35:47 +0800

    update packages

---
+++ evil-terminal-cursor-changer-20260405.711/evil-terminal-cursor-changer-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "evil-terminal-cursor-changer" "20260405.711"
+  "Change cursor shape and color in terminal."
+  ()
+  :url "https://github.com/7696122/evil-terminal-cursor-changer"
+  :commit "06adce5174cf97d500d250fa6053722a326c18db"
+  :revdesc "06adce5174cf"
+  :keywords '("terminal" "cursor" "evil"))
---
+++ modus-themes-20260413.1233/modus-themes-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "modus-themes" "20260413.1233"
+  "Elegant, highly legible and customizable themes."
+  '((emacs "28.1"))
+  :url "https://github.com/protesilaos/modus-themes"
+  :commit "657fd6f92d768f5b6da90f55b47b0d9e02467edb"
+  :revdesc "657fd6f92d76"
+  :keywords '("faces" "theme" "accessibility")
+  :authors '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainers '(("Protesilaos Stavrou" . "info@protesilaos.com")))
