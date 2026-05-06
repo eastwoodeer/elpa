61f5a226ec7f42b281a5c70f357a22c75578e4dc
eastwoodeer <eastwoodeer@outlook.com>
2026-05-06 21:16:16 +0800

    update packages

---
+++ evil-collection-20260505.29/evil-collection-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "evil-collection" "20260505.29"
+  "A set of keybindings for Evil mode."
+  '((emacs    "26.3")
+    (evil     "1.2.13")
+    (annalist "1.0"))
+  :url "https://github.com/emacs-evil/evil-collection"
+  :commit "95dca44dbe193a05ff714faf3732b92830695c40"
+  :revdesc "95dca44dbe19"
+  :keywords '("evil" "tools")
+  :authors '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers '(("James Nguyen" . "james@jojojames.com")))
---
+++ geiser-20260504.1659/geiser-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "geiser" "20260504.1659"
+  "GNU Emacs and Scheme talk to each other."
+  '((emacs   "27.1")
+    (project "0.8.1"))
+  :url "https://gitlab.com/emacs-geiser/"
+  :commit "06c3db4a053331e3d1fb8642a49c7a9b810cf5cb"
+  :revdesc "06c3db4a0533"
+  :keywords '("languages" "scheme" "geiser")
+  :authors '(("Jose Antonio Ortega Ruiz" . "(jao@gnu.org)"))
+  :maintainers '(("Jose Antonio Ortega Ruiz" . "(jao@gnu.org)")))
